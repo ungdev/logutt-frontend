d0afce58029e23155a1657d6b15c734c840a9ccd
Thomas Martignon <thomas.2905@hotmail.fr>
2021-04-04 16:51:11 +0200

    [Setup] route + service api

---
+++ src/common/config.js
@@ -0,0 +1,3 @@
+export const API_URL = "http://localhost:3000/";
+export const API_TOKEN = "";
+export default {API_URL, API_TOKEN};
--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./router";
 import vuetify from './plugins/vuetify';
+
+import ApiService from "./common/api.service";
 
 Vue.config.productionTip = false
 
+ApiService.init();
+
+// Ensure we checked user auth before each page load.
+router.beforeEach((to, from, next) => {
+  console.log("waiting for auth system");
+  next();
+});
+
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
