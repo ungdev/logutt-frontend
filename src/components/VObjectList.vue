ecd63a7765508650a3bc2f693405b943277bbd3e
Thomas Martignon <thomas.2905@hotmail.fr>
2021-04-13 16:59:03 +0200

    [Object] ajout logique component

---
+++ src/components/VObjectList.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <RwvObject v-for="(object) in objects" :object="object" :key="object.id"/>
+  </v-container>
+</template>
+
+<script>
+  import ObjectService from '../service/object.service';
+  import RwvObject from "./VObject";
+  export default {
+    name: "RwvObjectList",
+    components: {
+      RwvObject,
+    },
+    data() {
+      return {
+        objects: [],
+      }
+    },
+    mounted() {
+      ObjectService.get().then(res => this.objects = res.data);
+    }
+    
+  }
+</script>
