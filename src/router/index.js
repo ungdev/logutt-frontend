1c68cafbfca7b5d174dea3fd42c37098a42f19e9
OscarLeclerc <oscar.leclerc@hotmail.fr>
2021-04-13 19:26:37 +0200

    style: ajout d'une page d'accueil

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ export default new Router({
     routes: [
     {
       path: "/",
-      component: () => import("@/components/HelloWorld.vue"),
+      component: () => import("@/views/Home.vue"),
     }
   ]
 });
