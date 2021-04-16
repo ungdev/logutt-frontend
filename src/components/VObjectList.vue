09179a9fc8c7a976e6b4d70fcde596c3b9a0e6a6
Thomas Martignon <thomas.2905@hotmail.fr>
2021-04-16 22:40:59 +0200

    [Object] premier jet du composant objectList

--- src/components/VObjectList.vue
+++ src/components/VObjectList.vue
@@ -1,20 +1,85 @@
 <template>
-  <v-container>
-    <RwvObject v-for="(object) in objects" :object="object" :key="object.id"/>
-  </v-container>
+  <v-data-table 
+    :headers="headers" 
+    :items="objects" 
+    :items-per-page="5" 
+    :search="search"
+  >
+  <template v-slot:top>
+    <v-toolbar flat>
+      <v-toolbar-title>Object List</v-toolbar-title>
+      <v-divider class="mx-4" inset vertical></v-divider>
+      <v-text-field 
+        v-model="search" 
+        append-icon="mdi-magnify" 
+        label="Search" 
+        hide-details
+      ></v-text-field>
+      <v-combobox
+        v-model="selectOwners"
+        :items="owners"
+        label="Owner"
+        multiple
+        outlined
+        dense
+        hide-details
+      ></v-combobox>
+      <v-combobox
+        v-model="selectCategories"
+        :items="categories"
+        label="Categories"
+        multiple
+        outlined
+        dense
+        hide-details
+      ></v-combobox>
+      <v-combobox
+        v-model="selectMaterielTypes"
+        :items="materielTypes"
+        label="Materiel types"
+        multiple
+        outlined
+        dense
+        hide-details
+      ></v-combobox>
+      <v-divider class="mx-4" inset vertical></v-divider>
+      <v-btn color="primary" dark class="mb-2">
+        New Item
+      </v-btn>
+    </v-toolbar>
+  </template>
+  </v-data-table>
 </template>
 
 <script>
   import ObjectService from '../service/object.service';
-  import RwvObject from "./VObject";
   export default {
     name: "RwvObjectList",
     components: {
-      RwvObject,
     },
     data() {
       return {
+        search: '',
+        selectOwners: '',
+        selectCategories: '',
+        selectMaterielTypes: '',
+        headers: [
+          {
+            text: 'name',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Description', value: 'description' },
+          { text: 'Category id', value: 'category_id' },
+          { text: 'lendable', value: 'lendable' },
+          { text: 'created_at', value: 'created_at' },
+          { text: 'updated_at', value: 'updated_at' },
+        ],
         objects: [],
+        owners: ['test', 'cerf'],
+        categories: ['test', 'cerf'],
+        materielTypes: ['test', 'cerf'],
       }
     },
     mounted() {
