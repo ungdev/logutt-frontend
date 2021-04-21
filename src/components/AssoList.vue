a3a986a38d50493a2d2accb9dc8c637e582bcc7e
OscarLeclerc <oscar.leclerc@hotmail.fr>
2021-04-21 13:59:37 +0200

    [ Assos ] Affichage de la liste des assos

--- src/components/AssoList.vue
+++ src/components/AssoList.vue
@@ -1,23 +1,134 @@
 <template>
-  <v-row justify="center">
-    <v-expansion-panels popout>
-      <v-expansion-panel
-        v-for="(item,i) in 5"
-        :key="i"
-      >
-        <v-expansion-panel-header>Item</v-expansion-panel-header>
-        <v-expansion-panel-content>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
-  </v-row>
+  <div class="listeAsso">
+    <v-row justify="center">
+      <v-expansion-panels popout>
+        <v-expansion-panel v-for="asso in listeAssos" :key="asso.nomAsso">
+          <v-expansion-panel-header> {{asso.nomAsso}} </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <!-- Tableau des membres de l'assoc qui ont des accès sur LogUTT -->
+            <v-simple-table dense>
+              <template v-slot:default>
+                <thead>
+                  <tr>
+                    <th class="text-left"> Nom Prénom</th>
+                    <th class="text-left">
+                      Email
+                    </th>
+                  <th class="text-left">
+                    Rôle
+                  </th>
+                  <th class="text-left">
+                    Actions
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="etu in membres" :key="etu.name">
+                  <td>{{ etu.name }}</td>
+                  <td>{{ etu.email }}</td>
+                  <td>{{ etu.role }}</td>
+                  <td>Modifier --- Supprimer</td>
+                </tr>
+              </tbody>
+              <p>Mettre un bouton pour ajouter un etu à l'asso</p>
+              </template>
+            </v-simple-table>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-row>
+  </div> 
 </template>
 
 
 <script>
-  export default {
-    name: 'AssoList',
-  }
+export default {
+  name: 'AssoList',
+
+  data () {
+    return {
+      listeAssos:[
+        {
+          nomAsso: "BDE UTT",
+        },
+        {
+          nomAsso: "GALA UTT",
+        },
+        {
+          nomAsso: "UNG",
+        },
+        {
+          nomAsso: "Média",
+        },
+        {
+          nomAsso: "Argentique",
+        },
+        {
+          nomAsso: "Puls",
+        },
+        {
+          nomAsso: "Genius",
+        },
+        {
+          nomAsso: "Junior Conseil UTT",
+        },
+      ],
+      membres: [
+        {
+          name: 'Frozen Yogurta',
+          email: "cerf@utt.fr",
+          role: "administrateur",
+        },
+        {
+          name: 'Frozen Yozgurt',
+          email: "cerf@utt.fr",
+          role: "administrateur",
+        },
+        {
+          name: 'Frozen Yoguert',
+          email: "cerf@utt.fr",
+          role: "administrateur",
+        },
+        {
+          name: 'Frozen Yogrurt',
+          email: "cerf@utt.fr",
+          role: "administrateur",
+        },
+        {
+          name: 'Frozen Ytogurt',
+          email: "cerf@utt.fr",
+          role: "administrateur",
+        },
+        {
+          name: 'Frozen Yogyurt',
+          email: "cerf@utt.fr",
+          role: "administrateur",
+        },
+        {
+          name: 'Frozen Yoguurt',
+          email: "cerf@utt.fr",
+          role: "administrateur",
+        },
+        {
+          name: 'Frozen Yoguirt',
+          email: "cerf@utt.fr",
+          role: "administrateur",
+        },
+      ],
+    }
+  },
+}
 
 </script>
+
+<style lang="scss">
+  .listeAsso {
+    width: 90%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  v-form {
+    background-color: white;
+  }
+</style>
--- src/views/ViewAssoList.vue
+++ src/views/ViewAssoList.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <TheNavbar/>
+
+    <v-form ref="form" class="form-new-asso" lazy-validation>
+      <v-text-field class="field-name" id="inputName" label="Nom nouvelle association" required></v-text-field>
+
+      <v-btn color="success" class="btn btn-add mr-4" @click="newAsso"> + </v-btn>
+    </v-form>
+
     <AssoList/>
   </div>
 
@@ -17,11 +24,33 @@
     components:{
       AssoList,
       TheNavbar
-    }
-  }
+    },
 
+    methods: {
+      newAsso: function () {
+        var saisie = document.getElementById("inputName").value;
+        if(saisie) {
+          console.log(saisie);
+        } else {
+          console.log("impossible champs vide")
+        }
+      }
+    },
+  }
 </script>
 
 <style lang="scss">
-  
+  .form-new-asso {
+    display: flex;
+    width: 20%;
+  }
+
+  .field-name {
+    width: 90%;
+  }
+
+  .btn-add {
+    width: 10%;
+  }
+
 </style>
