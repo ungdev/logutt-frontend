03057ae2076ff73046147667563d9d3676801f08
Thomas Martignon <thomas.2905@hotmail.fr>
2021-04-29 14:50:38 +0200

    fix [Object] mise en composant

---
+++ src/components/ObjectListEmile.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-container class="grey lighten-3">
+    <v-row>
+      <v-col
+        cols="12"
+        sm="4"
+      >
+        <v-sheet
+          min-height="20vh"
+          rounded="lg"
+        >
+          <!-- SUB CARD START -->
+          <v-text-field label="Rechercher" filled rounded style="padding: 10px;"></v-text-field>
+
+          <v-row align="center" justify="space-around">
+            <v-btn text><v-icon left>mdi-close</v-icon>Filtres</v-btn>
+            <v-btn depressed color="primary">Nouveau</v-btn>
+          </v-row>
+          <!-- SUB CARD END -->
+        </v-sheet>
+      </v-col>
+
+      <v-col
+        cols="12"
+        sm="8"
+      >
+        <v-sheet
+          min-height="70vh"
+          rounded="lg"
+        >
+          <!-- MAIN CARD START -->
+            <v-list
+              subheader
+              two-line
+            >
+              <v-list-item
+                v-for="folder in objets"
+                :key="folder.title"
+              >
+                <v-list-item-avatar>
+                  <v-icon
+                    class="grey lighten-1"
+                    dark
+                  >
+                    mdi-folder
+                  </v-icon>
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                  <v-list-item-title v-text="folder.title"></v-list-item-title>
+
+                  <v-list-item-subtitle v-text="folder.subtitle"></v-list-item-subtitle>
+                </v-list-item-content>
+
+                <v-list-item-action>
+                  <v-btn icon>
+                    <v-icon color="grey lighten-1">mdi-information</v-icon>
+                  </v-btn>
+                </v-list-item-action>
+              </v-list-item>
+            </v-list>
+          <!-- MAIN CARD STOP -->
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: "ObjectList",
+    data: () => ({
+      links: [
+        'Matériel',
+        'Calendrier',
+        'Prêts',
+      ],
+      objets: [
+        {
+          title: 'Appareil à crêpe',
+          subtitle: 'Quantité : 3',
+        },
+        {
+          title: 'Aspirateur',
+          subtitle: 'Quantité : 2',
+        },
+        {
+          title: 'Gillets jaune',
+          subtitle: 'Quantité : 34',
+        },
+      ],
+    }),
+  }
+</script>
+
+<style lang="scss">
+  
+</style>
--- src/views/ViewObjectList.vue
+++ src/views/ViewObjectList.vue
@@ -1,92 +1,15 @@
 <template>
-  <v-main class="grey lighten-3">
-      <v-container>
-        <v-row>
-          <v-col
-            cols="12"
-            sm="4"
-          >
-            <v-sheet
-              min-height="20vh"
-              rounded="lg"
-            >
-              <!-- SUB CARD START -->
-              <v-text-field label="Rechercher" filled rounded style="padding: 10px;"></v-text-field>
-
-              <v-row align="center" justify="space-around">
-                <v-btn text><v-icon left>mdi-close</v-icon>Filtres</v-btn>
-                <v-btn depressed color="primary">Nouveau</v-btn>
-              </v-row>
-              <!-- SUB CARD END -->
-            </v-sheet>
-          </v-col>
-
-          <v-col
-            cols="12"
-            sm="8"
-          >
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-            >
-              <!-- MAIN CARD START -->
-                <v-list
-                  subheader
-                  two-line
-                >
-
-                  <v-list-item
-                    v-for="folder in objets"
-                    :key="folder.title"
-                  >
-                    <v-list-item-avatar>
-                      <v-icon
-                        class="grey lighten-1"
-                        dark
-                      >
-                        mdi-folder
-                      </v-icon>
-                    </v-list-item-avatar>
-
-                    <v-list-item-content>
-                      <v-list-item-title v-text="folder.title"></v-list-item-title>
-
-                      <v-list-item-subtitle v-text="folder.subtitle"></v-list-item-subtitle>
-                    </v-list-item-content>
-
-                    <v-list-item-action>
-                      <v-btn icon>
-                        <v-icon color="grey lighten-1">mdi-information</v-icon>
-                      </v-btn>
-                    </v-list-item-action>
-                  </v-list-item>
-                </v-list>
-              <!-- MAIN CARD STOP -->
-            </v-sheet>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
+  <ObjectList/>
 </template>
 
 <script>
+  import ObjectList from '../components/ObjectListEmile.vue';
   export default {
-    data: () => ({
-      objets: [
-        {
-          title: 'Appareil à crêpe',
-          subtitle: 'Quantité : 3',
-        },
-        {
-          title: 'Aspirateur',
-          subtitle: 'Quantité : 2',
-        },
-        {
-          title: 'Gillets jaune',
-          subtitle: 'Quantité : 34',
-        },
-      ],
-    }),
+    name: 'ViewObjectList',
+
+    components:{
+      ObjectList,
+    }
   }
 </script>
 
