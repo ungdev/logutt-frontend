db341a39a346d65d753cce81818ab0a88e1f91c1
Thomas Martignon <thomas.2905@hotmail.fr>
2021-05-07 15:27:27 +0200

    feat [Categorie] recherche + mise en composant

--- src/components/categorie/CategorieList.vue
+++ src/components/categorie/CategorieList.vue
@@ -1,105 +1,53 @@
 <template>
-  <v-container class="grey lighten-3">
-    <v-row>
-      <v-col cols="12" sm="4">
-        <v-sheet min-height="20vh" rounded="lg">
-          <!-- SUB CARD START -->
-          <v-text-field
-            v-model="search"
-            label="Rechercher"
-            filled
-            rounded
-            style="padding: 10px"
-          ></v-text-field>
+  <v-data-iterator
+    subheader
+    :items="categories"
+    item-key="id"
+    :search="search"
+  >
+    <template v-slot:default="{ items }">
+      <v-expansion-panels>
+        <v-expansion-panel
+          v-for="categorie in items" :key="categorie.id">
+          <v-expansion-panel-header>
+            {{ categorie.name }}
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+              <v-btn icon class="btn-icon">
+                        <v-icon color="grey lighten-1">mdi-square-edit-outline</v-icon>
+                      </v-btn>
+            <!-- Icon de suppression d'un élément -->
+            <VIconSuppression database="listeCategorie" :donnees=categorie />
 
-          <v-row align="center" justify="space-around">
-            <v-dialog v-model="formNew" persistent max-width="600px">
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn color="primary" dark v-bind="attrs" v-on="on">
-                  <v-icon> mdi-plus </v-icon> Nouvelle categorie
-                </v-btn>
+            <!-- Tableau des membres de l'assoc qui ont des accès sur LogUTT -->
+            <v-simple-table dense>
+              <template v-slot:default>
+                <thead>
+                  <tr>
+                    <th class="text-left">Nom de la sous catégorie</th>
+                    <th class="text-left">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Enceinte</td>
+                    <td>
+                      <v-btn icon class="btn-icon">
+                        <v-icon color="grey lighten-1">mdi-square-edit-outline</v-icon>
+                      </v-btn>
+                      <!-- Icon de suppression d'une sous catégorie -->
+                      <VIconSuppression database="listeCategorie" :donnees=categorie /> <!-- A MODIFIER POUR SUPP UNE SOUS CATÉ -->
+                    </td>
+                  </tr>
+                </tbody>
+                <p>Mettre un bouton pour ajouter une sous catégorie</p>
               </template>
-              <v-card>
-                <v-card-title>
-                  <span class="headline">Nouvelle categorie</span>
-                </v-card-title>
-                <v-card-text>
-                  <v-container>
-                    <v-row>
-                      <v-col cols="12" sm="6" md="6">
-                        <v-text-field
-                          v-model="name"
-                          label="Nom de la categorie*"
-                          required
-                          clearable
-                        ></v-text-field>
-                      </v-col>
-                    </v-row>
-                  </v-container>
-                  <small><span class="star-required">*</span> indiquez les champs requis</small>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="closeModal">
-                    Annuler
-                  </v-btn>
-                  <v-btn color="blue darken-1" :disabled="isDisable" text @click="newCategorie">
-                    Ajouter
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </v-row>
-          <!-- SUB CARD END -->
-        </v-sheet>
-      </v-col>
-
-      <v-col cols="12" sm="8">
-          <v-row justify="center" class="categorieList">
-            <v-expansion-panels popout>
-              <v-expansion-panel
-                v-for="categorie in this.categories" :key="categorie.id">
-                <v-expansion-panel-header>
-                  {{ categorie.name }}
-                </v-expansion-panel-header>
-                <v-expansion-panel-content>
-                    <v-btn icon class="btn-icon">
-                              <v-icon color="grey lighten-1">mdi-square-edit-outline</v-icon>
-                            </v-btn>
-                  <!-- Icon de suppression d'un élément -->
-                  <VIconSuppression database="listeCategorie" :donnees=categorie />
-
-                  <!-- Tableau des membres de l'assoc qui ont des accès sur LogUTT -->
-                  <v-simple-table dense>
-                    <template v-slot:default>
-                      <thead>
-                        <tr>
-                          <th class="text-left">Nom de la sous catégorie</th>
-                          <th class="text-left">Actions</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>Enceinte</td>
-                          <td>
-                            <v-btn icon class="btn-icon">
-                              <v-icon color="grey lighten-1">mdi-square-edit-outline</v-icon>
-                            </v-btn>
-                            <!-- Icon de suppression d'une sous catégorie -->
-                            <VIconSuppression database="listeCategorie" :donnees=categorie /> <!-- A MODIFIER POUR SUPP UNE SOUS CATÉ -->
-                          </td>
-                        </tr>
-                      </tbody>
-                      <p>Mettre un bouton pour ajouter une sous catégorie</p>
-                    </template>
-                  </v-simple-table>
-                </v-expansion-panel-content>
-              </v-expansion-panel>
-            </v-expansion-panels>
-          </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+            </v-simple-table>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </template>
+  </v-data-iterator>
 </template>
 
 <script>
@@ -111,11 +59,11 @@ export default {
   components: {
     VIconSuppression
   },
+  props: {
+    search: String,
+  },
   data() {
     return {
-      formNew: false,
-      name: "",
-      search: "",
       headers: [
         {
           text: "Nom de la catégorie",
@@ -127,31 +75,11 @@ export default {
       categories: [],
     };
   },
+  methods: {
+  },
   mounted() {
     CategoryService.get().then((res) => (this.categories = res.data));
   },
-
-  methods: {
-    newCategorie: function () {
-      CategoryService.post({ name: this.name });
-      this.closeModal();
-    },
-
-    closeModal: function () {
-          this.formNew = false;
-          this.name = '';
-      }
-  },
-
-  computed: {
-    isDisable: function() {
-      if(this.name == '') {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  }
 };
 </script>
 
--- src/views/ViewCategorieList.vue
+++ src/views/ViewCategorieList.vue
@@ -1,18 +1,47 @@
 <template>
-  <div>
-    <CategorieList/>
-  </div>
+  <list>
+    <template v-slot:searchBar>
+      <categorie-filter @set-filters="filtersChanged" @open-dialog="openDialog"/>
+    </template>
+    <categorie-list :search="search"/>
+    <edit-categorie v-model="selectedCategorie" :dialog="dialog" @close="closeDialog"/>
+  </list>
 </template>
 
 <script>
+import CategorieFilter from '../components/categorie/CategorieFilter.vue';
 import CategorieList from "../components/categorie/CategorieList";
+import EditCategorie from '../components/categorie/EditCategorie.vue';
+import List from "../components/List";
 
 export default {
   name: "ViewCategorieList",
-  
   components: {
-    CategorieList
+    CategorieList,
+    List,
+    CategorieFilter,
+    EditCategorie,
   },
+  data: () => ({
+    search: '',
+    dialog: false,
+    selectedCategorie: {}
+  }),
+  methods: {
+    filtersChanged(field) {
+      this.search = field;
+    },
+    openDialog(categorie) {
+      // TODO vrai traitement
+      this.selectedCategorie = categorie;
+      this.dialog = true;
+    },
+    closeDialog(categorie) {
+      // TODO vrai traitement
+      console.log(categorie);
+      this.dialog = false;
+    }
+  }
 };
 </script>
 
