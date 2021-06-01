8b2f0489711f2ebb20d0778d5028715846138e0a
OscarLeclerc <oscar.leclerc@hotmail.fr>
2021-06-01 13:14:46 +0200

    Refactor : modifier import

--- src/components/object/ObjectList.vue
+++ src/components/object/ObjectList.vue
@@ -116,8 +116,9 @@
 
 <script>
 import {object as ObjectService} from "../../service/logUTT.service";
-import CategoryService from "../../service/logutt/category.service";
-  export default {
+import {category as CategoryService} from "../../service/logUTT.service";
+
+export default {
     name: "ObjectList",
     props: {
       search: String,
